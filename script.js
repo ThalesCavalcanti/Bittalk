83295207097d7f346f026380e10ff693abd82ca4
Sami Carvalho <samigabriele1@gmail.com>
2026-05-14 19:26:30 -0300

    Ajustando redimensionamento e variaveis

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".perfil");
+const perfil = document.querySelector(".perfil");
+const overlay = document.querySelector(".perfil-overlay");
+const menuBtn = document.querySelector(".hamburger");
 
-hamburger.addEventListener("click", () => {
-  menu.classList.toggle("active");
+menuBtn.addEventListener("click", () => {
+  perfil.classList.toggle("active");
+
+  overlay.classList.toggle("active");
 });
 
-document.addEventListener("keydown", (e) => {
+/* Quando clicado, fecha */
 
-  if (e.key === "Escape") {
-    menu.classList.remove("active");
+overlay.addEventListener("click", () => {
+  perfil.classList.remove("active");
+
+  overlay.classList.remove("active");
+});
+
+/* Fecha quando tá na tela desktop */
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1025) {
+    perfil.classList.remove("active");
+
+    overlay.classList.remove("active");
   }
-
 });
 
 document.getElementById("year").textContent = new Date().getFullYear();
